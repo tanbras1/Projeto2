7c588573e8a88349b6ec38341776e238756c911d
tanbras1 <151696235+tanbras1@users.noreply.github.com>
2025-01-05 01:22:59 -0300

    Update ProdutosDAO.java

--- LeiloesSat/src/leiloessat/ProdutosDAO.java
+++ LeiloesSat/src/leiloessat/ProdutosDAO.java
@@ -193,35 +193,4 @@ e.printStackTrace();
         
 }
 
-/*import java.sql.PreparedStatement;
-import java.sql.Connection;
-import javax.swing.JOptionPane;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-
-
-public class ProdutosDAO {
-    
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
-    }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}*/
 
